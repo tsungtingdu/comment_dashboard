e19023fd58a05934a54c5952288c9816888ec02a
canopus <tsungtingdu@gmail.com>
2018-04-05 21:59:24 +0800

    1. Install Bootstrap 2. User can't upload duplicate comments 3. User can see commnet list at index page

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,42 @@
+class PagesController < ApplicationController
+
+  def index
+    @comments = Comment.all.order(lh_comment_id: :desc)
+  end
+
+  def read
+    csv_text = File.read('comment_20180405195735.csv')
+    csv = CSV.parse(csv_text, :headers => true)
+    csv.each do |row|
+      if Comment.where(:lh_comment_id => row[0]).any?
+        puts "No new entry"
+      else
+        new_comment = Comment.new  
+        new_comment.lh_comment_id = row[0]
+        new_comment.user_name = row[1]
+        new_comment.section_id = row[3]
+        new_comment.section_name = row[4]
+        new_comment.lesson_id = row[5]
+        new_comment.lesson_name = row[6]
+        new_comment.unit_id = row[7] 
+        new_comment.unit_name = row[8]
+        new_comment.assignment_id = row[9]
+        new_comment.title = row[11]
+        new_comment.content = row[12]
+        new_comment.replied_comment_id = row[13]
+        new_comment.replied_comment_title = row[14]
+        new_comment.replied_comment_content = row[15]
+        new_comment.upvote = row[16]
+        new_comment.total_following = row[17]
+        new_comment.date_comment = row[18]
+        new_comment.time_comment = row[19]
+        new_comment.thread_type = row[20]
+        new_comment.save
+      end                      
+    end
+    render :index
+  end   
+
+end
+
+
