c0e18a61eb930b7e585a0c52c0e9ab061db8f4bf
canopus <tsungtingdu@gmail.com>
2018-04-14 10:26:01 +0800

    create a comment_statuses model

---
+++ app/models/comment_status.rb
@@ -0,0 +1,2 @@
+class CommentStatus < ApplicationRecord
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180405120443) do
+ActiveRecord::Schema.define(version: 20180414022424) do
+
+  create_table "comment_statuses", force: :cascade do |t|
+    t.string "comment_status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "comments", force: :cascade do |t|
     t.bigint "lh_comment_id"
