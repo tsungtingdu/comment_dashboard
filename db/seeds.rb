4e7dc986d53c285d35fd53a23119693662f634d0
canopus <tsungtingdu@gmail.com>
2018-04-14 10:33:19 +0800

    create seed data for comment status

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+CommentStatus.destroy_all
+
+comment_status_list = [
+  {name: "不需回覆"},
+  {name: "未回覆"},
+  {name: "已回覆"},
+  {name: "已回復，列入 Lighthouse Issue List"},
+  {name: "已回覆，已修正"},    
+]
+
+comment_status_list.each do |status|
+  CommentStatus.create(comment_status: status[:name])
+end  
+
+puts "Commnet_status created"
